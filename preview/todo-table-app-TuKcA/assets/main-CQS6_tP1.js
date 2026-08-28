d4d063e98e71111252b6c03573e4f1b233ec5053
jock3 <jock3@users.noreply.github.com>
2026-08-28 13:31:50 +0000

    deploy: 6eb2bc365e8f0ea06df2b94f216ce985c097ff71

---
+++ preview/todo-table-app-TuKcA/assets/main-CQS6_tP1.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css                        */import{c as o,h as i,a as s}from"./supabase-DEtOZO0_.js";window.__hub={login:s,logout:i,session:o};
